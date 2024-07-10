16b316a5b2e4f341893c51cc9fda9b0254009b8c
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-10 07:54:05 +0530

    Update dependencies and add OnBoarding component

--- client/screens/onboarding/onboarding.screen.tsx
+++ client/screens/onboarding/onboarding.screen.tsx
@@ -1,12 +1,25 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useFonts, Raleway_700Bold } from '@expo-google-fonts/raleway';
+import { Nunito_400Regular, Nunito_700Bold } from '@expo-google-fonts/nunito';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function OnBoardingScreen() {
+
+    let [fontsLoaded, fontError] = useFonts({
+        Raleway_700Bold,
+        Nunito_400Regular,
+        Nunito_700Bold,
+    });
+
+    if (!fontsLoaded && !fontError) {
+        return null;
+    }
+
   return (
-    <View>
-      <Text>onboarding.screen</Text>
-    </View>
+    <LinearGradient colors={['#e5ecf9', '#f6f7f9']} style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+
+    </LinearGradient>
   )
 }
 
-const styles = StyleSheet.create({})
