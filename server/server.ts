428fa34f4f85dea1d83c0940614ded7163043ef4
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-11 17:49:47 +0530

    Update server configuration and dependencies

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import { app } from './app';
+import connectDB from './utils/db';
 require('dotenv').config();
 
 
@@ -6,5 +7,7 @@ require('dotenv').config();
 // Create a new server instance
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port :${process.env.PORT}`);
+    connectDB();
 }
 );
+
