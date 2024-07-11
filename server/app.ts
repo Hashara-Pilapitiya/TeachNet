e4818291aeee81b1b50f9ad145984ede01bb8061
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-11 16:43:09 +0530

    Update server configuration and dependencies

--- server/app.ts
+++ server/app.ts
@@ -1,2 +1,35 @@
-import express from 'express';
+require('dotenv').config();
+import express, { NextFunction, Request, Response } from 'express';
 export const app = express();
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+// Body parser
+app.use(express.json({ limit: '50mb' }));
+
+// Cookie parser
+app.use(cookieParser());
+
+// Enable CORS
+app.use(cors(
+    {
+        origin: process.env.ORIGIN,
+        credentials: true
+    }
+));
+
+// Testing API
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+      succcess: true,
+      message: "API is working",
+    });
+});
+
+// Unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+    const err = new Error(`Route ${req.originalUrl} not found`) as any;
+    err.statusCode = 404;
+    next(err);
+});
