e38c9ea20a4e6f4eb7ab02b45277210fbc3d153b
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-11 17:55:23 +0530

    Add Redis configuration for server

---
+++ server/utils/redis.ts
@@ -0,0 +1,13 @@
+import { Redis } from "ioredis";
+require("dotenv").config();
+
+const redisClient = () => {
+    if(process.env.REDIS_URL){
+        console.log("Redis Connected");
+        return process.env.REDIS_URL;
+    }
+    throw new Error("Redis URL not found");
+}
+
+
+export const redis = new Redis(redisClient());
