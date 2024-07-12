04dd9d398b5081a95c310628fc78309e68f43ee2
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-12 10:10:47 +0530

    Update npm dependencies for nodemailer and add SMTP configuration for Gmail

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -6,6 +6,7 @@ import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
+import sendMail from "../utils/sendMail";
 
 
 // A Register User
@@ -39,6 +40,24 @@ export const registerUser = CatchAsyncError(async (req: Request, res: Response, 
           const data = {user: {name: user.name}, activationCode};
 
           const html = await ejs.renderFile(path.join(__dirname, '../mails/activation-mail.ejs'), data);
+
+          try {
+            await sendMail({
+                email: user.email,
+                subject: 'Account Activation',
+                template: 'activation-mail.ejs',
+                data: {user: {name: user.name}, activationCode}
+            });
+
+            return res.status(200).json({
+                success: true,
+                message: `Please check your email: ${user.email} to activate your account`,
+                activationToken: activationToken.token
+            });
+
+          } catch (error: any) {
+              return next(new ErrorHandler(error.message, 400));
+          }
 
     
     } catch (error: any) {
