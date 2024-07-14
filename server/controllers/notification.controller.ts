4cdd25b39ed947dd0e4b3b759219c135e8e39a30
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-14 16:54:55 +0530

    Add updateNotification endpoint and controller

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -19,3 +19,31 @@ export const getNotifications = CatchAsyncError(async (req: Request, res: Respon
         next(new ErrorHandler(error.message, 500));
     }
 });
+
+
+
+// Update notification status
+export const updateNotification = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+
+        const notification = await NotificationModel.findById(req.params.id);
+
+        if (!notification) {
+            return next(new ErrorHandler(404, 'Notification not found'));
+        } else {
+            notification.status ? notification.status = 'read' : notification.status = 'unread';
+        }
+
+        await notification.save();
+
+        const notifications = await NotificationModel.find().sort({ createdAt: -1 });
+
+        res.status(200).json({
+            success: true,
+            notifications
+        });
+
+    } catch (error: any) {
+        next(new ErrorHandler(error.message, 500));
+    }
+});
--- server/routes/notification.route.ts
+++ server/routes/notification.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { getNotifications } from '../controllers/notification.controller';
+import { getNotifications, updateNotification } from '../controllers/notification.controller';
 
 const notificationRouter = express.Router();
 
 notificationRouter.get('/get-notifications/all', getNotifications);
+
+notificationRouter.get('/update-notification/:id', updateNotification);
 
 export default notificationRouter;
