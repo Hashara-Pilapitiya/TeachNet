69d742721b1e0ad588fd726dc9d3b35bd758a0e9
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-14 17:16:26 +0530

    Add UUID package and update dependencies

--- server/controllers/notification.controller.ts
+++ server/controllers/notification.controller.ts
@@ -2,6 +2,7 @@ import NotificationModel from "../models/notification.model";
 import { Request, Response, NextFunction } from "express";
 import { CatchAsyncError } from "../middleware/catchAsyncErrors";
 import ErrorHandler from "../utils/ErrorHandler";
+import cron from "node-cron";
 
 
 // Get all notifications
@@ -46,4 +47,15 @@ export const updateNotification = CatchAsyncError(async (req: Request, res: Resp
     } catch (error: any) {
         next(new ErrorHandler(error.message, 500));
     }
+});
+
+
+
+// Delete notification
+cron.schedule('0 0 0 * * *', async () => {
+    const thirtyDaysAgo = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+
+    await NotificationModel.deleteMany({  status: 'read', createdAt: { $lte: thirtyDaysAgo } });
+
+    console.log('Notifications deleted');
 });
