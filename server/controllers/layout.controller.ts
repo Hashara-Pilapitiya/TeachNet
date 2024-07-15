d9911cecad4c20d0924d90812e058aa4df564499
Hashara-Pilapitiya <hasharanethmi2020@gmail.com>
2024-07-15 07:43:10 +0530

    Add support for editing website layout

--- server/controllers/layout.controller.ts
+++ server/controllers/layout.controller.ts
@@ -69,3 +69,76 @@ export const createLayout = CatchAsyncError(async (req: Request, res: Response, 
         next(new ErrorHandler(error.message, 500));
     }
 });
+
+
+
+
+// Edit Layout
+export const editLayout = CatchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { type } = req.body;
+
+        if (type == "Banner") {
+
+            const bannerData: any = await LayoutModel.findOne({ type: "Banner" });
+
+            const { image, title, subtitle } = req.body;
+
+            if (bannerData) {
+                await cloudinary.v2.uploader.destroy(bannerData.image.public_id);
+            }
+
+            const myCloud = await cloudinary.v2.uploader.upload(image, {
+                folder: "layout"
+            });
+            
+            const banner = {
+                image: {
+                    public_id: myCloud.public_id,
+                    url: myCloud.secure_url
+                },
+                title,
+                subtitle
+            }
+
+            await LayoutModel.findByIdAndUpdate(bannerData._id, {banner});
+        }
+
+        if (type == "FAQ") {
+            const { faq } = req.body;
+
+            const faqData: any = await LayoutModel.findOne({ type: "FAQ" });
+
+            const faqItem = await Promise.all(faq.map(async (item: any) => {
+                return {
+                    question: item.question,
+                    answer: item.answer
+                }
+            }));
+
+            await LayoutModel.findByIdAndUpdate(faqData?._id, { faq: faqItem , type: "FAQ" });
+        }
+
+        if (type == "Categories") {
+            const { categories } = req.body;
+
+            const categoryData: any = await LayoutModel.findOne({ type: "Categories" });
+
+            const categoryItem = await Promise.all(categories.map(async (item: any) => {
+                return {
+                    title: item.title
+                }
+            }));
+
+            await LayoutModel.findByIdAndUpdate(categoryData?._id, { categories: categoryItem, type: "Categories" });
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Layout updated successfully"
+        });
+
+    } catch (error: any) {
+        next(new ErrorHandler(error.message, 500));
+    }
+});
--- server/routes/layout.route.ts
+++ server/routes/layout.route.ts
@@ -1,8 +1,10 @@
 import express from 'express';
-import { createLayout } from '../controllers/layout.controller';
+import { createLayout, editLayout } from '../controllers/layout.controller';
 
 const layoutRouter = express.Router();
 
 layoutRouter.post('/create-layout', createLayout);
+
+layoutRouter.post('/edit-layout', editLayout);
 
 export default layoutRouter;
